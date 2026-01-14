c337843ad870f547debd6447a986b5f5fb187156
psjw <dlrudtn108@gmail.com>
2026-01-14 21:03:42 +0900

    init project

---
+++ src/main/java/com/psjw/springbatchex/SpringBatchExApplication.java
@@ -0,0 +1,13 @@
+package com.psjw.springbatchex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchExApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/psjw/springbatchex/SpringBatchExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psjw.springbatchex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
